01af3a48d400db785291328fdf712caf4ff55833
FrancescoCina <francesco.cina04@gmail.com>
2021-10-11 15:56:50 +0200

    Collegato Bootstrap e fatto header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Home Page')
+
+@section('content')
+    {{-- <div class="bg-primary h-100">Prova</div> --}}
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,30 @@
+<header>
+    <div class="header-top d-flex justify-content-end bg-main-color pe-3 pt-2">
+        <h6 class="text-uppercase text-white fw-bold">Dc Power Visa Additional ds Sites</h6>
+    </div>
+    <div class="container">
+        <div class="header-bottom py-5 d-flex justify-content-between aling-items-center">
+            <div class="logo">
+                <img class="img-fluid" width=75 src="{{ asset('images/dc-logo.png') }}" alt="DC Comics">
+            </div>
+            <div class="header-links">
+                <ul class="d-flex">
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Tv</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Collectibles</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">Fans</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Shop</a></li>
+                </ul>
+            </div>
+            <div class="search-input">
+                <input type="search" placeholder="Search Comic">
+            </div>
+        </div>
+    </div>
+
+</header>
